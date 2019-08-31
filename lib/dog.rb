9ecd8bdfb12250e1bde3614c76bdcdf958a8d800
Emilia ☆ <emilia.mena26@gmail.com>
2019-08-31 22:14:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,26 @@
+# class Dog 
+
+# attr_accessor :name 
+
+#   def bark 
+#     puts "woof!"
+#   end
+
+# end
+
 class Dog 
-
-attr_accessor :name 
-
-  def bark 
+  
+  def name=(name)
+    @name=name
+  end
+  
+  def name
+    @name 
+  end
+  
+  def bark
     puts "woof!"
   end
 
-end
+
+end 
